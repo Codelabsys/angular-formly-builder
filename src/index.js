5e2fc44b5dcd2431ad1587248471729f1c0c392b
Abbas <ralas1953@cuvox.de>
2017-06-13 16:10:36 +0200

    drop zone field

--- src/index.js
+++ src/index.js
@@ -13,10 +13,9 @@
             controller: function ($scope) {
                 // check if it was defined.  If not - set a default
                 $scope.item = $scope.item || { name: $scope.name };
-
             },
             compile: function (tElement, attrs, transclude) {
-
+                tElement.removeAttr('builder-field-type');
                 tElement.attr('dnd-draggable', 'item');
                 tElement.attr('dnd-effect-allowed', 'move');
                 return {
@@ -45,13 +44,43 @@
         };
     }]);
 
-    formBuilder.directive('builderDropzoneField', ['$compile', function ($compile) {
+    formBuilder.directive('builderDropzoneField', ['$compile', 'builderConfig', '$q', '$templateCache', '$http', function ($compile, builderConfig, $q, $templateCache, $http) {
         return {
             restrict: 'AE',
             scope: {
+                item: '='
             },
-            templateUrl: 'none'
+            link: function (scope, elem, attr) {
+                let fieldConfig = builderConfig.getType(scope.item.name);
+                getFieldTemplate(fieldConfig).then(function (templateString) {
+                    elem.append(templateString);
+                }).catch(function (error) {
+                    throw new Error('can not load template ' + error);
+                });
+            },
+            controller: function ($scope) {
+                //TODO run controllers
+            },
         };
+
+        function getFieldTemplate(component) {
+            if (angular.isUndefined(component.template) && angular.isUndefined(component.templateUrl))
+                throw new Error('component must have a template or a templateUrl');
+            else {
+                let templatePromise = null;
+                if (component.template)
+                    return templatePromise = $q.when(component.template);
+                else if (component.templateUrl) {
+                    templatePromise = $q.when(component.templateUrl);
+                    const httpOptions = { cache: $templateCache };
+                    return templatePromise.then((url) => $http.get(url, httpOptions))
+                        .then((response) => response.data)
+                        .catch(function (error) {
+                            throw new Error('can not load template url ' + error);
+                        });
+                }
+            }
+        }
     }]);
 
     formBuilder.directive('builderFieldConfig', ['$compile', function ($compile) {
@@ -63,5 +92,25 @@
         };
     }]);
 
+    formBuilder.factory('builderConfig', ['formlyConfig', function (formlyConfig) {
+        const typeMap = {};
+        return {
+            setType: function (component) {
+                if (angular.isObject(component)) {
+                    //checkType(component);
+                    typeMap[component.name] = component;
+                } else {
+                    throw new Error('Whoops!');
+                }
+            },
+            getType: function (name) {
+                if (name)
+                    return typeMap[name];
+                else {
+                    throw new Error('Whoops!');
+                }
+            }
+        };
+    }]);
 
 })(angular.module('formBuilder', []));
